ef4d7fd0a4e2234cefe6ebbc62160d4ec1bcd964
James Njuguna <jamesnjugunan998@gmail.com>
2024-07-25 03:48:24 +0300

    db connection

--- controllers/AdminController.ts
+++ controllers/AdminController.ts
@@ -1,9 +1,25 @@
 import  {Request, Response, NextFunction} from 'express';
 import { CreateVandorInput } from '../dto';
+import { Vandor} from '../models'
 
 
 export const CreateVandor = async (req:Request, res:Response, next:NextFunction) => {
   const {name, address, pincode,foodType, email, password, ownerName, phone} = <CreateVandorInput>req.body;
+
+  const CreateVandor = await Vandor.create({
+     name: name,
+     address: address,
+     pincode: pincode,
+     foodType: foodType,
+     email: email,
+     password: password,
+     salt: '',
+     ownerName:ownerName,
+     phone: phone,
+     rating: 0,
+     serviceAvailable: false,
+     coverImage: [],
+  })
 
   return res.json({name, address, pincode,foodType, email, password, ownerName, phone})
 
