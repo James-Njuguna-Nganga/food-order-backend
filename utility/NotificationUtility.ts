7a094b191642df63237506d5c2413ea09e554b69
James Njuguna <jamesnjugunan998@gmail.com>
2024-07-27 11:01:44 +0300

    twilio setting

--- utility/NotificationUtility.ts
+++ utility/NotificationUtility.ts
@@ -7,16 +7,16 @@ export const GenerateOtp = () => {
   
 }
 
-export const onRequestOTP = async (otp: number, toPhoneNumber: string) => {
-  const accountSid = "AC861a4936e850902ee05e6cc03f8fe3b9";
-  const authToken = "f721c27c7fe11c0e6004ba8f7356ccd4";
-  const client = require('twilio')(accountSid, authToken);
+// export const onRequestOTP = async (otp: number, toPhoneNumber: string) => {
+//   // const accountSid = "AC861a4936e850902ee05e6cc03f8fe3b9";
+//   const authToken = "f721c27c7fe11c0e6004ba8f7356ccd4";
+//   const client = require('twilio')(accountSid, authToken);
 
-  const response = await client.messages.create({
-      body: 'Your OTP is ${otp}',
-      from: '+254799619817',
-      to: `+254${toPhoneNumber}`,
-  });
+//   const response = await client.messages.create({
+//       body: 'Your OTP is ${otp}',
+//       from: '+254799619817',
+//       to: `+254${toPhoneNumber}`,
+//   });
 
-  return response;
-}
+//   return response;
+// }
